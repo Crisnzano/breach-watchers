8791ec678356a9748d7e578093ab74418d38a7a9
Crispus <crispus.nzano@gmail.com>
2024-10-11 13:49:32 +0300

    feat: add sign up and sign in

--- components/pages/dashboard/Checklist.tsx
+++ components/pages/dashboard/Checklist.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React from 'react'
 import { useState } from 'react'
 import { CheckCircle, Circle, ChevronDown, ChevronUp, FileText, CalendarIcon } from 'lucide-react'
 import { Button } from "@/components/ui/button"
